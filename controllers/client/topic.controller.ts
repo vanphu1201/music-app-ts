41377ff13f3436606cb0dfd50636a9b209398a2a
vanphuTteam <vanphu120107@gmail.com>
2026-01-19 10:20:42 +0700

    cấu hình lại folder pug

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -8,6 +8,6 @@ export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({ deleted: false });
     console.log(topics);
     res.render("client/pages/topics/index.pug", {
-
+        pageTitle: "Chủ đề bài hát"
     });
 }

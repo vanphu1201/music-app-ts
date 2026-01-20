28ab9c547610a895f5aff5b9f3e7690eda8d5666
vanphuTteam <vanphu120107@gmail.com>
2026-01-20 09:45:11 +0700

    làm trang chủ chủ đề bài hát

--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -8,6 +8,7 @@ export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({ deleted: false });
     console.log(topics);
     res.render("client/pages/topics/index.pug", {
-        pageTitle: "Chủ đề bài hát"
+        pageTitle: "Chủ đề bài hát",
+        topics: topics
     });
 }
